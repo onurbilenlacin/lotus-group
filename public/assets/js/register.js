6c7d3dce068aeab893d10010b498b35c7dcb68bd
onurbilenlacin <onurbilenlacin@gmail.com>
2024-06-22 16:13:30 +0300

    project initialized

---
+++ public/assets/js/register.js
@@ -0,0 +1,29 @@
+function handleSubmit() {
+    console.log("asdasdasdasd");
+    // const email = document.getElementById("email").value;
+    // const password = document.getElementById("password").value;
+    // const confirmPassword =
+    //     document.getElementById("confirmPassword").value;
+    // if (password !== confirmPassword) {
+    //     alert("Passwords do not match");
+    // } else {
+    //     fetch("https://reqres.in/api/register", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify({
+    //             email,
+    //             password
+    //         })
+    //     })
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             if (data.error) {
+    //                 alert(data.error);
+    //             } else {
+    //                 alert("User registered successfully");
+    //             }
+    //         });
+    // }
+}
